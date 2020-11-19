465d9d82bd604079e2547231bbe56dcc74ff290d
Ellie Lambert <ebezjian@gmail.com>
2020-11-18 19:35:01 -0500

    try to add calendar api
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- src/calendarAPI.js
+++ src/calendarAPI.js
@@ -2,7 +2,7 @@
 export default class CalendarHoliday {
   static async getHoliday(){
     try {
-      const response = await fetch(`https://holidayapi.com/v1/holidays/?pretty&key=${process.env.CALENDER_API_KEY}&country=US&year=2019, {mode: 'no-cors }`);
+      const response = await fetch(`https://holidayapi.com/v1/holidays/?pretty&key=25c11a57-a9d6-47e2-bdc2-968cb70cdc44&country=US&year=2019`, {mode: 'no-cors' });
       if(!response.ok) {
         throw Error(response.statusText);
       }
--- src/main.js
+++ src/main.js
@@ -9,11 +9,11 @@ import CalendarHoliday from './calendarAPI.js';
 
 function getElements(response, holidayResponse) {
   const convertTemp = ((response.main.temp - 273.15) * 9/5 + 32).toFixed(0);
-  if (response.main) {
+  if (response.main && holidayResponse.status) {
     $('.airTemp').text(`The current air temperature is ${convertTemp} degrees Farhrenheit`); 
     $('.windSpeed').text(`The current wind speed is ${response.wind.speed} m/s`);
     $('.forecast').text(`Todays forecast is ${response.weather[0].description}`);
-    $('.holidaze').text(`Holidays in 2019: ${holidayResponse.holidays[0][1][2]}`);
+    $('.holidaze').text(`Holidays in 2019: ${holidayResponse.holidays[0].name}`);
   } else {
     $('.showErrors').text(`There was an error: ${response}`);
     $('.showErrors').text(`There was an error: ${holidayResponse}`);
